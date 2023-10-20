252303d43092959342a1914e892ac90005c80af2
BigKeaner <122934425+BigKeaner@users.noreply.github.com>
2023-10-19 20:12:15 -0400

    Create run.py

---
+++ Code/Server/run.py
@@ -0,0 +1,98 @@
+from random import randint
+import time
+from Motor import *
+import Led
+import Light
+from Ultrasonic import *
+
+MIN_DISTANCE = 10  # Minimum distance to trigger object detection
+Car = Motor()
+ultrasonic = Ultrasonic()
+
+# Initial state
+state = "Driving"
+
+def move_forward(distance):
+    Car.setMotorModel(distance*10,distance*10,distance*10,distance*10)
+    time.sleep(1)
+def object_detected():
+    return True if ultrasonic.getDistance() > 0
+def detected_distance():
+    return ultrasonic.getDistance()
+def stop():
+    Car.setMotorModel(0,0,0,0)
+    time.sleep(5)
+    state = "Field of View Examination"
+def turn_left()
+    Car.setMotorModel(500,0,-500,0)
+    time.sleep(1)
+def turn_right()
+    Car.setMotorModel(-500,0,500,0)
+    time.sleep(1)
+def scan(direction):
+    if direction is "left":
+        car.on_btn_Turn_Left()
+    elif direction is "right":
+        car.on_btn_Turn_Right()
+    elif direction is "back":
+        Car.setMotorModel(distance*-10,distance*-10,distance*-10,distance*-10)
+        time.sleep(1)
+    else:
+        stop()
+
+def valid_path_found():
+    return True if not object_detected or detected_distance < MIN_DISTANCE else state is "stop"
+
+
+
+# Main program loop
+while True:
+    if state == "Drive":
+        # Move the car forward
+        if valid_path_found():
+            if detected_distance() > 100:
+                move_forward(100)
+            else:
+                move_forward(detected_distance - MIN_DISTANCE) 
+        # Check for object detection
+        else:
+            state = "stop"
+
+    elif state == "Stop":
+        # Stop the car
+        stop()
+        
+        state = "Field of View Examination"
+
+    elif state == "Field of View Examination":
+        # Rotate the servos to examine the field of view
+        determinant = randint(0,1)
+        if determinant < 1:
+            scan("left")
+            if valid_path_found():
+                turn_left()
+                state = "Drive"
+            if state != "Drive":
+                scan("right")
+                if valid_path_found():
+                    turn_right()
+                    state = "Drive"
+                if state != "Drive":
+                    scan("back")
+                    if valid_path_found():
+                        state = "Field of View Examination"
+        else:
+            scan("right")
+            if valid_path_found():
+                turn_right()
+                state = "Drive"
+            if state != "Drive":
+                scan("left")
+                if valid_path_found():
+                    turn_left()
+                    state = "Drive"
+                if state != "Drive":
+                    scan("back")
+                    if valid_path_found():
+                        state = "Field of View Examination"
+
